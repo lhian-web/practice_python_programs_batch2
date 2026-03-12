45a689186930d84330f23c52183d6ad43979f3f1
unknown <jhullianchica@gmail.com>
2026-03-12 20:30:47 +0800

    Added input, take remainder, and print the remainder

--- program5_prints_the_remainder_of_first_number_divided_by_second_number.py
+++ program5_prints_the_remainder_of_first_number_divided_by_second_number.py
@@ -1,3 +1,6 @@
 # Input two numbers
-# Calculate remainder
-# Print remainder
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+remainder = num1 % num2 # Calculate remainder
+print("The remainder is", remainder) # Print remainder
