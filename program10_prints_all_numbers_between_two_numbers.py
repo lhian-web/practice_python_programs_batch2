87bb3445ef3e996b4c8f7ad2f3cb568bbd79e1bc
unknown <jhullianchica@gmail.com>
2026-03-12 21:51:34 +0800

    Added input, for loop for numbers between two numbers, print numbers between

--- program10_prints_all_numbers_between_two_numbers.py
+++ program10_prints_all_numbers_between_two_numbers.py
@@ -1,3 +1,8 @@
 # Input two numbers
+num1 = int(input("Enter starting number: "))
+num2 = int(input("Enter ending number: "))
+
 # Loop through numbers between num1 and num2
-# Print numbers between the two numbers
+for i in range(num1 + 1, num2):
+    # Print numbers between the two numbers
+    print(i)
