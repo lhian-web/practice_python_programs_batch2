cfa8d41adf682bc957c1a0dbfd6e332379c792bc
unknown <jhullianchica@gmail.com>
2026-03-12 21:27:07 +0800

    Added while loop, checked if number is odd, printed odd numbers

--- program8_prints_all_odd_numbers_from_zero_to_a_hundred.py
+++ program8_prints_all_odd_numbers_from_zero_to_a_hundred.py
@@ -1,4 +1,7 @@
 # variable starting with zero
-# check if number is odd
-# print odd number
-# go to next number
+num = 0
+
+while num < 100:
+    if num % 2 != 0: # check if number is odd
+        print(num)# print odd number
+    num += 1 # go to next number
