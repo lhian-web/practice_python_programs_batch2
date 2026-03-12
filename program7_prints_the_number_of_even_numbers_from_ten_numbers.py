e5c0c47e7986f5f839256ef4270ef66e071d123f
unknown <jhullianchica@gmail.com>
2026-03-12 21:01:21 +0800

    Added variable, Input numbers, checked if numbers is even, printed number of even

--- program7_prints_the_number_of_even_numbers_from_ten_numbers.py
+++ program7_prints_the_number_of_even_numbers_from_ten_numbers.py
@@ -1,4 +1,11 @@
 # Even count variable
+even_count = 0
+
 # Input numbers
-# Check if number is even
-# Print even count
+for i in range(10):
+    num = float(input("Enter a number: "))
+    if num % 2 == 0: # Check if number is even
+        even_count += 1
+
+print("The number of even numbers is", even_count) # Print even count
+
