aac6cadcc57774dfc74c991556427cff89bda90f
unknown <jhullianchica@gmail.com>
2026-03-12 19:39:07 +0800

    Added input, take difference, and print difference

--- program3_prints_the_difference_of_two_numbers.py
+++ program3_prints_the_difference_of_two_numbers.py
@@ -1,3 +1,6 @@
 # Input two numbers
-# Take the difference of two numbers
-# Print the difference
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+difference = num1 - num2 # Take the difference of two numbers
+print("The difference between", num1, "and", num2, "is", difference) # Print the difference
