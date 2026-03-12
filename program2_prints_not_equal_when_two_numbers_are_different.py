9fd21ed94b0b5d1e3bbac5afd2446e46e60bf57c
unknown <jhullianchica@gmail.com>
2026-03-12 19:33:48 +0800

    Added Input, checked if numbers are equal, print 'Not equal'and 'Both numbers are equal'

--- program2_prints_not_equal_when_two_numbers_are_different.py
+++ program2_prints_not_equal_when_two_numbers_are_different.py
@@ -1,4 +1,9 @@
 # Input two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
 # Check if numbers are equal
-# Print "Not equal" when numbers are not equal
-# Print "Both numbers are equal"
+if num1 != num2:
+    print("Not equal") # Print "Not equal" when numbers are not equal
+else:
+    print("Both numbers are equal") # Print "Both numbers are equal"
